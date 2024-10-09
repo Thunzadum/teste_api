4e38c4e48c6ff522af8f28dbd169cfdf0db45780
Thunzadum <danielglima1977@gmail.com>
2024-10-08 23:46:54 -0300

    Update server.js

--- server.js
+++ server.js
@@ -2,6 +2,10 @@ const express = require('express');
 
 const server = express();
 
+server.use('/ping', (req, res) => {
+    res.send('pong');
+});
+
 server.listen(3001, () => {
     console.log('Servidor rodando na porta 3001');
 });
