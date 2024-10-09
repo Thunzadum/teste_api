260be6ef307ae6be7ef5ce281a5611578ae540b1
Thunzadum <danielglima1977@gmail.com>
2024-10-09 00:22:34 -0300

    Update server.js

--- server.js
+++ server.js
@@ -8,10 +8,6 @@ server.use('/', (req, res) => {
     res.send('Olá Daniel!');
 });
 
-server.use('/ping', (req, res) => {
-    res.send('pong');
-});
-
 server.listen(process.env.PORT, () => {
     console.log('Servidor rodando....');
 });
