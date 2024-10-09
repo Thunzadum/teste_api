2106c4d7a9841f274c8438e3104da62a718c4cc5
Thunzadum <danielglima1977@gmail.com>
2024-10-09 00:53:46 -0300

    update

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 require('dotenv').config()
 const express = require('express');
-
+const ApiRoute = require('./src/routes/routes');
 const server = express();
 
 server.use(express.json());
@@ -9,17 +9,7 @@ server.use(express.urlencoded({extended: true}));
 
 
 
-server.use('/ping', (req, res) => {
-    res.send('Pong!');
-});
-
-server.use('/ola', (req, res) => {
-    res.send('Olá Daniel!');
-});
-
-server.use('/', (req, res) => {
-    res.send('Olá!');
-});
+server.use('/', ApiRoute);
 
 server.listen(process.env.PORT, () => {
     console.log('Servidor rodando....');
