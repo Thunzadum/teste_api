73bf3e934aa5f14145e8be1754043bf9afeb415b
Thunzadum <danielglima1977@gmail.com>
2024-10-08 23:42:08 -0300

    Update server.js

--- server.js
+++ server.js
@@ -1,17 +1,7 @@
 const express = require('express');
 
-const app = express();
+const server = express();
 
-var port = process.env.PORT || 80
-
-app.get('/', (req, res) =>{
-    res.send('Olá Mundo!');
-});
-
-app.get('/player', (req, res) =>{
-    res.send('Olá Player!');
-});
-
-app.listen(port, () => {
-    console.log('Server Iniciado...');
+server.listen(3001, () => {
+    console.log('Servidor rodando na porta 3001');
 });
