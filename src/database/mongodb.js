2325beeb6ae5aa72984bbafac3cde3f091d7579b
Thunzadum <danielglima1977@gmail.com>
2024-10-09 13:00:11 -0300

    Update mongodb.js

--- src/database/mongodb.js
+++ src/database/mongodb.js
@@ -5,7 +5,7 @@ require('dotenv').config();
 const connectdb = async () => {
     try {
         console.log('Conectando ao mongodb...');
-        await mongoose.connect("mongodb+srv://showlimashow1978:9jUNY9mOtudyQvUj@cluster0.z7j6p.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0");
+        await mongoose.connect(process.env.MONGOURL);
         console.log('Mongodb conectado com sucesso');
     }catch(error) {
         console.log('Erro ao conectar ao mongodb: '+ error);
