fa80a5c762bf8988d4de78383c44e2786701f8b6
Thunzadum <danielglima1977@gmail.com>
2024-10-09 21:42:58 -0300

    update

--- src/controllers/apicontroller.js
+++ src/controllers/apicontroller.js
@@ -7,38 +7,49 @@ module.exports = {
     },
 
     signup: async(req, res) => {
-        let{
-            email,
+        let {
+            //avatar,
+            //nome,
             nick,
+            email,
             password,
             moedas,
-            videos
+            videos,
+            //score,
+            //ranking,
+            //timeGame
         } = req.body;
 
-        const userExist = await User.findOne({email});
+
+        const userExist = await User.findOne({ email });
         if(userExist) {
             res.json({
                 data: [],
-                error: 'Usuário já Existe'
+                error: 'Usuário inválido'
             });
             return;
         }
-
+        
         const passwordHash = await bcrypt.hash(password, 10);
         const newUser = new User({
-            email,
+            //avatar,
+            //nome,
             nick,
+            email,
             passwordHash,
             moedas,
-            videos
+            videos,
+            //score,
+            //ranking,
+            //timeGame
         });
 
         const userSave = await newUser.save();
         if(userSave) {
             res.json({
                 data: userSave,
-                msg: '',
-                error: ''
+                msg: "",
+                //error: ''
             });
             return;
         } else {
@@ -76,6 +87,181 @@ module.exports = {
             data: userExist,
             msg: ""
             //error: ''
+        });
+    },
+    moedas: async(req, res) => {
+        const nick = req.params.nick;
+        const newMoedas = req.params.moedas;
+        const user = await User.findOne({nick}).exec();
+        if(!user) {
+            res.json({
+                error: 'Usuário Inválido!'
+            });
+            return;
+        }
+
+        const id = user._id;
+        const moedasAtual = user.moedas;
+        if(newMoedas > moedasAtual || newMoedas < moedasAtual) {
+            const userUpdate = await User.findByIdAndUpdate(id, {moedas: newMoedas});
+            if(!userUpdate) {
+                res.json({error: 'Error ao realizar update!'});
+            }
+            res.json({
+                data: [],
+                msg: 'Moedas alteradas com sucesso'
+            })
+        } else {
+            res.json({
+                data: [],
+                msg: ''
+            });
+            return;
+        }
+    },
+    videos: async(req, res) => {
+        const nick = req.params.nick;
+        const newVideos = req.params.videos;
+        const user = await User.findOne({nick}).exec();
+        if(!user) {
+            res.json({
+                error: 'Usuário Inválido!'
+            });
+            return;
+        }
+
+        const id = user._id;
+        const videosAtual = user.videos;
+        if(newVideos > videosAtual || newVideos < videosAtual) {
+            const userUpdate = await User.findByIdAndUpdate(id, {videos: newVideos});
+            if(!userUpdate) {
+                res.json({error: 'Error ao realizar update!'});
+            }
+            res.json({
+                data: [],
+                msg: 'Videos alterados com sucesso'
+            })
+        } else {
+            res.json({
+                data: [],
+                msg: ''
+            });
+            return;
+        }
+    },
+    delete: function(req, res, next){
+        var query = { nick: req.params.nick}
+        var value = req.body;
+        User.findOneAndDelete(query, value).then(data => {
+            res.json({
+                msg: 'Usuário deletado com sucesso'
+            });
+        });
+    },
+    info: async(req, res) => {
+        const email = req.params.email;
+        const userInfo = await User.findOne({email})
+        //.select({nick: 1, avatar: 1, ranking: 1, score: 1, _id: 0})
+        .select({nick: 1, _id: 0})
+        .exec();
+        if(!userInfo) {
+            res.json({
+                data: [],
+                error: 'Usuario nao encontrado'
+            });
+            return;
+        }
+        res.json({
+            userInfo
+        });
+    },
+    infomoedas: async(req, res) => {
+        const nick = req.params.nick;
+        const userInfo = await User.findOne({nick})
+        //.select({nick: 1, avatar: 1, ranking: 1, score: 1, _id: 0})
+        .select({moedas: 1, _id: 0})
+        .exec();
+        if(!userInfo) {
+            res.json({
+                data: [],
+                error: 'Usuario nao encontrado'
+            });
+            return;
+        }
+        res.json({
+            userInfo
+        });
+    },
+    infovideos: async(req, res) => {
+        const nick = req.params.nick;
+        const userInfo = await User.findOne({nick})
+        .select({videos: 1, _id: 0})
+        .exec();
+        if(!userInfo) {
+            res.json({
+                data: [],
+                error: 'Usuario nao encontrado'
+            });
+            return;
+        }
+        res.json({
+            userInfo
+        });
+    },
+    update: async(req, res) => {
+        let {
+            avatar,
+            nome,
+            nick,
+            email
+        } = req.body;
+
+        const id = req.params.id;
+
+        const user = await User.findByIdAndUpdate(id,{
+            nome,
+            avatar,
+            nick,
+            email
+        }).exec();
+        if(!user) {
+            res.json({
+                data: [],
+                error: 'Usuario invalido'
+            });
+            return;
+        }
+
+        res.json({
+            data: [],
+            msg: 'Usuario alterado com sucesso',
+            error: ''
+        });
+    },
+    highscore: async(req, res) => {
+        const user = await User.find({
+            ranking: 1,
+            score: {$gt: 0}
+        })
+        .sort({ranking: 1})
+        .limit(2)
+        .select({
+            //nick: 1,
+            //avatar: 1,
+            ranking: 1,
+            //score: 1,
+            nome: 1,
+            _id: 0
+        }).exec();
+        if(!user) {
+            res.json({
+                data: [],
+                msg: 'Erro ao realizar consulta'
+            });
+            return;
+        }
+        res.json({
+            user
         });
     },
 }
--- src/routes/routes.js
+++ src/routes/routes.js
@@ -3,8 +3,21 @@ const apicontroller = require('../controllers/apicontroller');
 const router = express.Router();
 
 
+//#region ######################### ROTAS APICONTROLLER
 router.get('/ping', apicontroller.ping);
+router.get('/info/:email', apicontroller.info);
+router.get('/infomoedas/:nick', apicontroller.infomoedas);
+router.get('/infovideos/:nick', apicontroller.infovideos);
+router.get('/user/:nome', apicontroller.info);
+router.get('/highscore', apicontroller.highscore);
+
 router.post('/signup', apicontroller.signup);
 router.post('/signin', apicontroller.signin);
+
+router.put('/update/:id', apicontroller.update);
+router.put('/user/:nick/moedas/:moedas', apicontroller.moedas);
+
+router.delete('/delete/:nick', apicontroller.delete);
+//#endregion
 
 module.exports = router;
