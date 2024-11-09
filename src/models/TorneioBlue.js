09082c21e6c4282562617de44a93aa9ee65a626b
Thunzadum <danielglima1977@gmail.com>
2024-11-09 17:04:33 -0300

    criado novo schema

---
+++ src/models/TorneioBlue.js
@@ -0,0 +1,19 @@
+const mongoose = require('mongoose');
+const modelSchema = new mongoose.Schema({
+    participantes: String,
+    //email: String,
+    //passwordHash: String,
+    nick: String,
+    qtdregistroTB: Number,
+    score: Number,
+    ranking: Number,
+    qtdparticipantes: Number,
+});
+
+const modelName = 'TorneioBlue';
+
+if(mongoose.connection && mongoose.connection.models[modelName]) {
+    module.exports = mongoose.connection.models[modelName];
+} else {
+    module.exports = mongoose.model(modelName, modelSchema);
+}
