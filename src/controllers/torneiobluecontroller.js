383f2a883d8fbd14e59559609745a68039faaccc
Thunzadum <danielglima1977@gmail.com>
2024-11-09 17:23:49 -0300

    atualiza T

---
+++ src/controllers/torneiobluecontroller.js
@@ -0,0 +1,43 @@
+const TorneioBlue = require('../models/TorneioBlue');
+const bcrypt = require('bcrypt');
+
+
+
+module.exports = {
+    qtdjogador: async(req, res) => {
+        let {
+            participantes,
+            qtdparticipantes,
+        } = req.body;
+
+
+        const userExist = await TorneioBlue.findOne({ participantes });
+        if(userExist) {
+            res.json({
+                data: [],
+                error: 'Usuário inválido'
+            });
+            return;
+        }
+        const newUser = new Torneio({
+            participantes,
+            qtdparticipantes,
+        });
+
+        const userSave = await newUser.save();
+        if(userSave) {
+            res.json({
+                data: userSave,
+                msg: "",
+                //error: ''
+            });
+            return;
+        } else {
+            res.json({
+                data: [],
+                error: 'Erro ao salvar usuário'
+            });
+            return;
+        }
+    }
+}
