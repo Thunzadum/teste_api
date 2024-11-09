947b9272b24823f3a08df77db05f139beacae355
Thunzadum <danielglima1977@gmail.com>
2024-11-09 17:46:31 -0300

    atualiza torneio

--- src/controllers/torneiobluecontroller.js
+++ src/controllers/torneiobluecontroller.js
@@ -19,7 +19,7 @@ module.exports = {
             });
             return;
         }
-        const newUser = new Torneio({
+        const newUser = new TorneioBlue({
             participantes,
             qtdparticipantes,
         });
